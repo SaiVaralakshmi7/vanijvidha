cf631e5f7c411ffc865103599fcbb85338e601f5
SaiVaralakshmi7 <99262624+SaiVaralakshmi7@users.noreply.github.com>
2023-01-11 16:40:18 +0530

    Add files via upload

---
+++ subtypes.js
@@ -0,0 +1,32 @@
+function aler(){
+    alert("do u want exit ?");
+   }
+
+   function openNav() {
+document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+document.getElementById("myNav").style.width = "0%";
+}
+
+   function on() {
+document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+document.getElementById("overlay").style.display = "none";
+}
+
+function lit1() {
+document.getElementById("btn1").style.color = "red";
+}
+function lit2() {
+document.getElementById("btn2").style.color = "red";
+}
+function lit3() {
+document.getElementById("btn3").style.color = "red";
+}
+function lit4() {
+document.getElementById("btn4").style.color = "red";
+}
